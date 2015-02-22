63b9eda321c2814926d5b3c6649af8a0e4445973
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-02-22 13:10:47 -0700

    SnapBot library

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='snapchat_bots',
+    version='0.1',
+    description='Library for building Snapchat bots',
+    long_description=open('README.md').read(),
+    author='Anastasis Germanidis',
+    author_email='agermanidis@gmail.com',
+    url='https://github.com/agermanidis/SnapchatBot',
+    packages=['snapchat_bots'],
+    install_requires=[
+        'schedule>=0.3.1',
+        'requests>=2.5.1',
+        'Pillow>=2.7.0',
+        'pysnap>=0.1.1'
+    ],
+    dependency_links = ['https://github.com/martinp/pysnap/tarball/master#egg=pysnap-0.1.1'],
+    license=open('LICENSE').read()
+)
